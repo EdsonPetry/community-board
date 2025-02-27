30e63cc94cb80cc20e8ca4c18623bc6c3deeeeb6
EdsonPetry <edsonlbpetry@gmail.com>
2025-02-27 15:29:31 -0500

    Fixing the commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
+import Card from "./components/Card"
+import "./index.css"
+import Canopy from "./assets/canopy.png"
+import Truck1 from "./assets/truck1.jpg"
+import Truck2 from "./assets/truck2.jpg"
+import Truck3 from "./assets/truck3.jpg"
 
 const App = () => {
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello World
+    <div className="bg-blue-300 flex flex-col justify-center items-center ml-50 mr-50 ">
+      <img src={Canopy} alt="canopy image" className="h-auto w-350 pb-30"></img>
+      <h1 className="text-white text-9xl pb-40">
+        Food Truck Favorites
       </h1>
+      <div className="flex justify-around gap-50 pb-15 pl-20 pr-20">
+        <Card image={Truck1} name="D'Albano" type="Mexican type"/>
+        <Card image={Truck2} name="Cube 55" type="Asian"/>
+        <Card image={Truck3} name="Tajeen Halal" type="Middle Eastern"/>
+      </div>
     </div>
   )
 }
